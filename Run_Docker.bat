14eec6f8be95867e0108536bfca7b3e9793d9387
arqdariogomez <arqdariogomez@gmail.com>
2025-12-21 17:41:38 -0600

    Fix: Aggressive Docker container removal in Run_Docker.bat

--- Run_Docker.bat
+++ Run_Docker.bat
@@ -21,7 +21,8 @@ docker run --rm --gpus all nvidia/cuda:12.1.0-base-ubuntu22.04 nvidia-smi
 
 echo.
 echo Starting Docker containers...
-docker compose down
+docker compose down --remove-orphans
+docker rm -f difflocks_studio >nul 2>&1
 docker compose up --build
 
 if %errorlevel% neq 0 (
